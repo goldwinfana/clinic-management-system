0464b72a66b47b7d9882bc5b268600962b564084
goldwinfana <66006971+goldwinfana@users.noreply.github.com>
2022-05-08 10:06:32 +0200

    may

---
+++ check_login.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+if((isset($_SESSION["email"]) && isset($_SESSION["password"]))){
+
+    if($_SESSION["user"]=='nurse'){
+        header("location:nurse/view-patient.php");
+    }
+
+//    if($_SESSION["user"]=='admin'){
+//        header("location:index.php");
+//    }
+//
+//    if($_SESSION["user"]=='admin'){
+//        header("location:index.php");
+//    }
+	$myemail = $_SESSION['email'];
+}else {
+	header("location:login.php");
+}
+?>
