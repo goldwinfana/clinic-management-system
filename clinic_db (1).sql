5866a9b165d914589ce5c75de5be8b06097a2fad
goldwinfana <66006971+goldwinfana@users.noreply.github.com>
2022-05-22 17:52:16 +0200

    Add files via upload

---
+++ clinic_db (1).sql
@@ -0,0 +1,413 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2022 at 05:51 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clinic_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(500) NOT NULL,
+  `email` text NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(500) NOT NULL,
+  `gender` varchar(500) NOT NULL,
+  `dob` text NOT NULL,
+  `mobileno` text NOT NULL,
+  `addr` varchar(500) NOT NULL,
+  `notes` varchar(200) NOT NULL,
+  `image` varchar(2000) NOT NULL,
+  `created_on` date NOT NULL,
+  `updated_on` date NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `last_login` date NOT NULL,
+  `delete_status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `fname`, `lname`, `gender`, `dob`, `mobileno`, `addr`, `notes`, `image`, `created_on`, `updated_on`, `role_id`, `last_login`, `delete_status`) VALUES
+(1, 'admin', 'admin@gmail.com', 'aa7f019c326413d5b8bcad4314228bcd33ef557f5d81c7cc977f7728156f4357', 'Admin', 'admin', 'Male', '2018-11-26', '9423979339', '<p>Maharashtra, India</p>\r\n', '<p>admin panel</p>\r\n', 'profile.jpg', '2018-04-30', '2019-10-15', 1, '0000-00-00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `departmentid` int(10) NOT NULL,
+  `departmentname` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `delete_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`departmentid`, `departmentname`, `description`, `status`, `delete_status`) VALUES
+(1, 'ICU department', 'ICU department for people with serious illness', 'Active', 0),
+(2, 'Neurology department', 'neurology department for treating diseases of nervous system', 'Active', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doctorid` int(10) NOT NULL,
+  `doctorname` varchar(50) NOT NULL,
+  `mobileno` varchar(15) NOT NULL,
+  `departmentid` int(10) NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `education` varchar(25) NOT NULL,
+  `experience` float(11,1) NOT NULL,
+  `consultancy_charge` float(10,2) NOT NULL,
+  `delete_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`doctorid`, `doctorname`, `mobileno`, `departmentid`, `email`, `password`, `status`, `education`, `experience`, `consultancy_charge`, `delete_status`) VALUES
+(1, 'Dr Kat', '9423979339', 1, 'doctor@gmail.com', 'aa7f019c326413d5b8bcad4314228bcd33ef557f5d81c7cc977f7728156f4357', 'Active', 'MD', 3.0, 200.00, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_timings`
+--
+
+CREATE TABLE `doctor_timings` (
+  `doctor_timings_id` int(10) NOT NULL,
+  `doctorid` int(10) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `available_day` varchar(15) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `delete_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctor_timings`
+--
+
+INSERT INTO `doctor_timings` (`doctor_timings_id`, `doctorid`, `start_time`, `end_time`, `available_day`, `status`, `delete_status`) VALUES
+(1, 1, '09:00:00', '12:00:00', '', 'Active', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_website`
+--
+
+CREATE TABLE `manage_website` (
+  `id` int(11) NOT NULL,
+  `business_name` varchar(600) NOT NULL,
+  `business_email` varchar(600) NOT NULL,
+  `business_web` varchar(500) NOT NULL,
+  `portal_addr` varchar(500) NOT NULL,
+  `addr` varchar(600) NOT NULL,
+  `curr_sym` varchar(600) NOT NULL,
+  `curr_position` varchar(500) NOT NULL,
+  `front_end_en` varchar(500) NOT NULL,
+  `date_format` date NOT NULL,
+  `def_tax` varchar(500) NOT NULL,
+  `logo` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `manage_website`
+--
+
+INSERT INTO `manage_website` (`id`, `business_name`, `business_email`, `business_web`, `portal_addr`, `addr`, `curr_sym`, `curr_position`, `front_end_en`, `date_format`, `def_tax`, `logo`) VALUES
+(1, 'Mayuri K', 'mayuri.infospace@gmail.com', '#', '#', '<p>Maharashtra, India</p>\r\n', '$', 'right', '0', '0000-00-00', '0.20', 'logo for hospital system.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nurse`
+--
+
+CREATE TABLE `nurse` (
+  `nurseid` int(11) NOT NULL,
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `mobile` text DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nurse`
+--
+
+INSERT INTO `nurse` (`nurseid`, `fname`, `lname`, `email`, `password`, `mobile`, `status`) VALUES
+(1, 'nurse', 'fisrt', 'nurse@gmail.com', 'aa7f019c326413d5b8bcad4314228bcd33ef557f5d81c7cc977f7728156f4357', '0810258254', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patientid` int(13) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` text NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `mobileno` varchar(15) NOT NULL,
+  `pincode` varchar(20) DEFAULT NULL,
+  `prescription` text DEFAULT NULL,
+  `confirm_collection` text DEFAULT NULL,
+  `temperature` text DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `bloodgroup` varchar(20) DEFAULT NULL,
+  `gender` varchar(10) NOT NULL,
+  `age` int(11) NOT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `delete_status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`patientid`, `fname`, `lname`, `address`, `mobileno`, `pincode`, `prescription`, `confirm_collection`, `temperature`, `email`, `password`, `bloodgroup`, `gender`, `age`, `status`, `delete_status`) VALUES
+(1, 'James Bond', '', 'Soshanguve', '9423979339', '1234', 'Panajwhjqwjqdhqjdqhwhdd  nmewfwfew', 'collected', '35,2', 'goldwinfana5@gmail.com', 'bbcff4db4d8057800d59a68224efd87e545fa1512dfc3ef68298283fbb3b6358', 'B+', 'Female', 19950725, 'Active', 0),
+(2147483647, 'test', 'test', '477 Sisulu Street', '0710217499', NULL, NULL, 'collected', NULL, 'test@gmail.com', 'fc921b5f45f0d6dbac22dddc57104001a817fde6bbb86cf1a339ca2bdc1a63f0', NULL, 'male', 24, 'inactive', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pharmacy`
+--
+
+CREATE TABLE `pharmacy` (
+  `pharmacyid` int(11) NOT NULL,
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `mobile` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pharmacy`
+--
+
+INSERT INTO `pharmacy` (`pharmacyid`, `fname`, `lname`, `email`, `password`, `status`, `mobile`) VALUES
+(1, 'pharmacy', 'pahrs', 'pharmacy@gmail.com', 'fc921b5f45f0d6dbac22dddc57104001a817fde6bbb86cf1a339ca2bdc1a63f0', 0, '0751245555');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `prescriptionid` int(10) NOT NULL,
+  `treatment_records_id` int(10) NOT NULL,
+  `doctorid` int(10) NOT NULL,
+  `patientid` int(10) NOT NULL,
+  `delivery_type` varchar(10) NOT NULL COMMENT 'Delivered through appointment or online order',
+  `delivery_id` int(10) NOT NULL COMMENT 'appointmentid or orderid',
+  `prescriptiondate` date NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `appointmentid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription_records`
+--
+
+CREATE TABLE `prescription_records` (
+  `prescription_record_id` int(10) NOT NULL,
+  `prescription_id` int(10) NOT NULL,
+  `medicine_name` varchar(25) NOT NULL,
+  `cost` float(10,2) NOT NULL,
+  `unit` int(10) NOT NULL,
+  `dosage` varchar(25) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_type`
+--
+
+CREATE TABLE `service_type` (
+  `service_type_id` int(10) NOT NULL,
+  `service_type` varchar(100) NOT NULL,
+  `servicecharge` float(10,2) NOT NULL,
+  `description` text NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `service_type`
+--
+
+INSERT INTO `service_type` (`service_type_id`, `service_type`, `servicecharge`, `description`, `status`) VALUES
+(10, 'X-ray', 250.00, 'To take fractured photo copy', 'Active'),
+(11, 'Scanning', 450.00, 'To scan body from injury', 'Active'),
+(12, 'MRI', 300.00, 'Regarding body scan', 'Active'),
+(13, 'Blood Testing', 150.00, 'To detect the type of disease', 'Active'),
+(14, 'Diagnosis', 210.00, 'To analyse the diagnosis', 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `loginname` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `patientname` varchar(50) NOT NULL,
+  `mobileno` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `createddateandtime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`departmentid`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doctorid`);
+
+--
+-- Indexes for table `doctor_timings`
+--
+ALTER TABLE `doctor_timings`
+  ADD PRIMARY KEY (`doctor_timings_id`);
+
+--
+-- Indexes for table `manage_website`
+--
+ALTER TABLE `manage_website`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nurse`
+--
+ALTER TABLE `nurse`
+  ADD PRIMARY KEY (`nurseid`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patientid`),
+  ADD KEY `loginid` (`email`);
+
+--
+-- Indexes for table `pharmacy`
+--
+ALTER TABLE `pharmacy`
+  ADD PRIMARY KEY (`pharmacyid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `departmentid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `doctorid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `doctor_timings`
+--
+ALTER TABLE `doctor_timings`
+  MODIFY `doctor_timings_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `manage_website`
+--
+ALTER TABLE `manage_website`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nurse`
+--
+ALTER TABLE `nurse`
+  MODIFY `nurseid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pharmacy`
+--
+ALTER TABLE `pharmacy`
+  MODIFY `pharmacyid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
